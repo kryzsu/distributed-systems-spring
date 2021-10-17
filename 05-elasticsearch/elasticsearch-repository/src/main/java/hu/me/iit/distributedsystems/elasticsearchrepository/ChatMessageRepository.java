c0213a51ae7a81ce62b89106bdf2666dbb92933a
Zoltan Krizsan <krizsan@iit.uni-miskolc.hu>
2021-10-17 21:21:44 +0200

    sender vagy uzenetben keres

--- 05-elasticsearch/elasticsearch-repository/src/main/java/hu/me/iit/distributedsystems/elasticsearchrepository/ChatMessageRepository.java
+++ 05-elasticsearch/elasticsearch-repository/src/main/java/hu/me/iit/distributedsystems/elasticsearchrepository/ChatMessageRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 
 public interface ChatMessageRepository extends ElasticsearchRepository<ChatMessage, String> {
     Page<ChatMessage> findBySender(String sender, Pageable pageable);
+    Page<ChatMessage> findBySenderOrMessage(String sender, String message, Pageable pageable);
+
 }
